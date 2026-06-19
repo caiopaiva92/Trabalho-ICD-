71548fbe0961e3cc9ef0ec7c12987649ffca8ffb
Thiago Santos de Lacerda <thiagostslacerda@gmail.com>
2026-06-19 10:47:30 -0300

    csv_creator melhorado para semiautomática

--- busca_dados_2020.py
+++ busca_dados_2020.py
@@ -1,6 +1,16 @@
 from csv_creator import csv_creator
 
-# Exemplo pegando apenas os sistemas operacionais de janeiro de 2020
-link = 'https://web.archive.org/web/20200202095248/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam'
-id = 'osversion_details'
-csv_creator(link, id, "Janeiro", 2020)
+# Exemplo pegando apenas os dados de 2020
+link_mes = [('https://web.archive.org/web/20200202095248/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Janeiro'), 
+           ('https://web.archive.org/web/20200316152704/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Fevereiro'), 
+           ('https://web.archive.org/web/20200407072208/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Março'), 
+           ('https://web.archive.org/web/20200505150638/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Abril'), 
+           ('https://web.archive.org/web/20200608014405/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Maio'), 
+           ('https://web.archive.org/web/20200808140607/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Julho'), 
+           ('https://web.archive.org/web/20200908144606/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Agosto'), 
+           ('https://web.archive.org/web/20201009143637/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Setembro'), 
+           ('https://web.archive.org/web/20201104192639/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Outubro'), 
+           ('https://web.archive.org/web/20201209234627/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Novembro'), 
+           ('https://web.archive.org/web/20210105073410/https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam', 'Dezembro')]
+
+csv_creator(link_mes, 2020)
--- csv_creator.py
+++ csv_creator.py
@@ -1,18 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-def csv_creator(link, id_, mes, ano):
-    html = requests.get(link).text
-    soup = BeautifulSoup(html, 'html.parser')
+def csv_creator(links_meses, ano):
+    ids_categorias = [('osversion_details', 'OS Version'), ('cat0_details', 'System RAM'), ('cat1_details', 'Intel CPU Speeds'), ('cat2_details', 'Physical CPUs'), ('cat3_details', 'Video Card Description'), ('cat4_details', 'VRAM'), ('cat5_details', 'Primary Display Resolution'), ('cat6_details', 'Multi-Monitor Desktop Resolution')]
 
-    # Pega apenas a div do id especificado (Os ids tem que ser os details para funcionar)
-    div = soup.find('div', id=f'{id_}')
+    with open(f'dados_{ano}.csv', 'w', encoding='utf-8') as f:
+                f.write('Categoria, Especificação, Porcentagens, Incrementos/Decrementos, Mês, Ano\n')
 
-    titulos = div.find_all('div', class_='stats_col_mid')
-    porcentagens = div.find_all('div', class_='stats_col_right')
-    incs_decs = div.find_all('div', class_='stats_col_right2')
+    for link, mes in links_meses:
+        time.sleep(3)
+        html = requests.get(link).text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    with open(f'dados_{ano}.csv', 'a+', encoding='utf-8') as f:
-        f.write('Título, Porcentagens, Incrementos/Decrementos, Mês, Ano\n')
-        for titulo, porcentagem, inc_dec in zip(titulos, porcentagens, incs_decs):
-            f.write(f"{titulo.text.strip()}, {porcentagem.text.strip()}, {inc_dec.text.strip()}, {mes}, {ano}\n")
+        for id_, categoria in ids_categorias:
+            # Pega apenas a div do id especificado (Os ids tem que ser os details para funcionar)
+            div = soup.find('div', id=f'{id_}')
+
+            titulos = div.find_all('div', class_='stats_col_mid')
+            porcentagens = div.find_all('div', class_='stats_col_right')
+            incs_decs = div.find_all('div', class_='stats_col_right2')
+
+            with open(f'dados_{ano}.csv', 'a+', encoding='utf-8') as f:
+                for titulo, porcentagem, inc_dec in zip(titulos, porcentagens, incs_decs):
+                    f.write(f"{categoria}, {titulo.text.strip()}, {porcentagem.text.strip()}, {inc_dec.text.strip()}, {mes}, {ano}\n")
